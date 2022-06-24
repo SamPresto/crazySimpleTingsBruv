e92f6358ae7890c870d7809f2653987f2aa1b28a
Sam Prestigiacomo <98630238+SamPresto@users.noreply.github.com>
2022-06-24 12:18:59 -0500

    Add files via upload
    monkey hide monkey see

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const player = document.querySelectorAll('#player');
+const open = document.querySelector('.open');
+const closed = document.querySelector('.closed');
 
+closed.addEventListener('click', () => {
+    if(open.classList.contains('open')) {
+        open.classList.add('active');
+        closed.classList.remove('active');
+    }
+});
 
-for(let person of player) {
-    person.addEventListener('click', () => {
-        person.remove();
-    });
-}
-
+open.addEventListener('click', () => {
+    if(closed.classList.contains('closed')) {
+        closed.classList.add('active');
+        open.classList.remove('active');
+    }
+})
